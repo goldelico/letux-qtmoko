0fbdd9377bb2760bc6cd9cc7a515f01bd75b9c44
Radek Polak <psonek2@seznam.cz>
2009-07-27 17:55:01 +0200

    QX - better messages in qx helper

--- src/3rdparty/applications/qx_helper/qx_helper.cpp
+++ src/3rdparty/applications/qx_helper/qx_helper.cpp
@@ -13,24 +13,24 @@ int main(int argc, char **argv)
 {
 	int  LinuxConsoleFd;
 	
-	fprintf(stderr, "VT hello started\n");
+	fprintf(stderr, "QX helper started\n");
 
     if ((LinuxConsoleFd = open("/dev/tty7", O_RDWR|O_NDELAY, 0)) < 0)
     {
-		perror("VT hello: Cannot open /dev/tty7");
+		perror("QX helper: Cannot open /dev/tty7");
 		return 0;
     }
     
     if (ioctl(LinuxConsoleFd, VT_ACTIVATE, 7) != 0)
     {
-		fprintf(stderr, "LinuxInit: VT_ACTIVATE failed\n");
+		fprintf(stderr, "QX helper: VT_ACTIVATE failed\n");
     }
-	fprintf(stderr, "Calling VT_WAITACTIVE for /dev/tty7\n");
+	fprintf(stderr, "QX helper: calling VT_WAITACTIVE for /dev/tty7\n");
     if (ioctl(LinuxConsoleFd, VT_WAITACTIVE, 7) != 0)
     {
-		fprintf(stderr, "LinuxInit: VT_WAITACTIVE failed\n");
+		fprintf(stderr, "QX helper: VT_WAITACTIVE failed\n");
     }
-    fprintf(stderr, "VT_WAITACTIVE done\n");
+    fprintf(stderr, "QX helper: VT_WAITACTIVE done\n");
     close(LinuxConsoleFd);
 	sleep(180);
 }
