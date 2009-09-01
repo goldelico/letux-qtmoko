13f05363ece49a2b6c33281153106690ee3bd33f
Radek Polak <psonek2@seznam.cz>
2009-09-01 07:00:10 +0200

    Added GPRS sharing script, menu entry and doc

---
+++ devices/neo/src/devtools/scripts/share-gprs.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+IPTABLES='/sbin/iptables'
+
+# Set interface values
+EXTIF='ppp0'
+INTIF='usb0'
+
+# enable ip forwarding in the kernel
+/bin/echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# flush rules and delete chains
+$IPTABLES -F
+$IPTABLES -X
+
+#Enable masquerading to allow LAN internet access
+$IPTABLES -t nat -A POSTROUTING -o $EXTIF -j MASQUERADE
+
+#Forward LAN traffic from LAN $INTIF to Internet $EXTIF
+$IPTABLES -A FORWARD -i $INTIF -o $EXTIF -m state --state NEW,ESTABLISHED -j ACCEPT
+
+#Allowing access to the SSH server"
+#$IPTABLES -A INPUT --protocol tcp --dport 22 -j ACCEPT
+
+#Allowing access to the HTTP server"
+#$IPTABLES -A INPUT --protocol tcp --dport 80 -j ACCEPT
+
+# block out all other Internet access on $EXTIF
+$IPTABLES -A INPUT -i $EXTIF -m state --state NEW,INVALID -j DROP
+$IPTABLES -A FORWARD -i $EXTIF -m state --state NEW,INVALID -j DROP
+
