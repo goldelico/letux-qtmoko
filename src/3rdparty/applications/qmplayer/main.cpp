a34cf85303c54e4462ffa792ead7410aab432700
Radek Polak <psonek2@seznam.cz>
2009-09-15 22:13:41 +0200

    Revert "Deleted debug code and fixed an error when compiling for a 64bit target"
    This reverts commit 32e1c131309d960792476ea269774c5c0795bf04.
    
    We need the code for the PC version of qmplayer. Will fix this
    with new define.

--- src/3rdparty/applications/qmplayer/main.cpp
+++ src/3rdparty/applications/qmplayer/main.cpp
@@ -1,5 +1,22 @@
+#ifdef QT_QWS_FICGTA01
+
 #include "qmplayer.h"
 #include <qtopiaapplication.h>
 QTOPIA_ADD_APPLICATION(QTOPIA_TARGET,QMplayer)
 QTOPIA_MAIN
 
+#else
+
+#include <QtGui/QApplication>
+#include "qmplayer.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QMplayer w;
+    w.show();
+    return a.exec();
+}
+
+
+#endif
