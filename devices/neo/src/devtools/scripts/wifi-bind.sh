ff5f5748ca8f1283d27ac751f5cf9fbd732f0aad
Radek Polak <psonek2@seznam.cz>
2010-02-10 09:20:34 +0100

    Fix wifi bind/unbind for 2.6.32 kernels

--- devices/neo/src/devtools/scripts/wifi-bind.sh
+++ devices/neo/src/devtools/scripts/wifi-bind.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo s3c2440-sdi > /sys/bus/platform/drivers/s3c2440-sdi/bind
+echo glamo-mci.0 > /sys/bus/platform/drivers/glamo-mci/bind
--- devices/neo/src/devtools/scripts/wifi-unbind.sh
+++ devices/neo/src/devtools/scripts/wifi-unbind.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo s3c2440-sdi > /sys/bus/platform/drivers/s3c2440-sdi/unbind
+echo glamo-mci.0 > /sys/bus/platform/drivers/glamo-mci/unbind
