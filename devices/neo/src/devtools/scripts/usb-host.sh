52d06be07edec2c3d0e03c0f9ffde60468496951
Radek Polak <psonek2@seznam.cz>
2010-04-07 16:50:02 +0200

    sysfs for usb device/host on 2.6.32

--- devices/neo/src/devtools/scripts/usb-host.sh
+++ devices/neo/src/devtools/scripts/usb-host.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-
-if [ -f /sys/class/i2c-adapter/i2c-0/0-0073/neo1973-pm-host.0/hostmode ]; then
-  echo 1 > /sys/class/i2c-adapter/i2c-0/0-0073/neo1973-pm-host.0/hostmode
-fi
-if [ -f /sys/devices/platform/s3c-ohci/usb_mode ]; then
-  echo host > /sys/devices/platform/s3c-ohci/usb_mode
-fi
+echo 1 > /sys/devices/platform/s3c2440-i2c/i2c-0/0-0073/pcf50633-gpio/reg-fixed-voltage.2/gta02-pm-usbhost.0/power_on
+echo host > /sys/devices/platform/s3c2410-ohci/usb_mode
