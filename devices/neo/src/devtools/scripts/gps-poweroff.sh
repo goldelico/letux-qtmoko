43a8b0dae6418cf00092ca78dbfab59d19460464
Piotr Gabryjeluk <piotr@gabryjeluk.pl>
2010-07-25 21:19:59 +0200

    Use AGPS and only put GPS in standby when GPS is not used

--- devices/neo/src/devtools/scripts/gps-poweroff.sh
+++ devices/neo/src/devtools/scripts/gps-poweroff.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-echo 0 > /sys/bus/platform/devices/neo1973-pm-gps.0/power_on
+echo "Putting GPS in stand-by mode"
+gta02-gps --setup-serial /dev/ttySAC1 --sleep
+ 
+# dump AGPS data if we have fix in 3 seconds from now
+if timeout 3 grep -m 1 'GPGSA,A,[23]' /dev/ttySAC1 1>/dev/null 2>&1; then
+        echo "We have fix. Dumping AGPS data"
+        gta02-gps --verbose --setup-serial /dev/ttySAC1 --dump-agps /var/cache/agpsdata
+        exit 0
+fi
+ 
+echo "No fix. Not dumping AGPS data"
--- devices/neo/src/devtools/scripts/gps-poweron.sh
+++ devices/neo/src/devtools/scripts/gps-poweron.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
-echo 1 > /sys/bus/platform/devices/neo1973-pm-gps.0/power_on
+# let om work
+. /opt/qtmoko/qpe.env
+ 
+# enable gps
+if [ "`om gps power`" = "0" ]; then
+        echo "Powering GPS on"
+        om gps power 1
+        echo "Supplying AGPS data"
+        gta02-gps --verbose --setup-serial /dev/ttySAC1 --load-agps /var/cache/agpsdata
+        echo "Setting GPS to be powered in suspend"
+        om gps keep-on-in-suspend 1
+else
+        echo "GPS powered on, waking up"
+        gta02-gps --setup-serial /dev/ttySAC1 --wake-up
+        echo "Supplying AGPS data"
+        gta02-gps --verbose --setup-serial /dev/ttySAC1 --load-agps /var/cache/agpsdata
+fi
+ 
+# return OK
+exit 0
