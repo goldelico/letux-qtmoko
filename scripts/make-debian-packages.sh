352895ccce008a6e675093f1340c087cb718f432
Radek Polak <psonek2@seznam.cz>
2011-02-16 14:25:03 +0100

    Update paths to build dir

--- scripts/make-debian-packages.sh
+++ scripts/make-debian-packages.sh
@@ -57,95 +57,95 @@ cd $qtmoko_dir/etc/themes/smart/ && dpkg-buildpackage -tc
 mv $qtmoko_dir/etc/themes/qtmoko-theme-smart_*-* $outdir
 
 echo "==================================================== Building qtmoko-codecs.deb"
-cd $qtmoko_dir/qtmoko-apps/codecs && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-codecs_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/plugins/codecs/codecs-package && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/plugins/codecs/qtmoko-codecs_*-* $outdir
 
 echo "==================================================== Building qtmoko-qgcide.deb"
-cd $qtmoko_dir/qtmoko-apps/qgcide && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-qgcide_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/qgcide && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-qgcide_*-* $outdir
 
 echo "=============================================== Building qtmoko-qtpedometer.deb"
-cd $qtmoko_dir/qtmoko-apps/qtpedometer && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-qtpedometer_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/qtpedometer && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-qtpedometer_*-* $outdir
 
 # echo "================================================== Building qtmoko-eyepiece.deb"
-# cd $qtmoko_dir/qtmoko-apps/eyepiece && dpkg-buildpackage -tc
-# mv $qtmoko_dir/qtmoko-apps/qtmoko-eyepiece_*-* $outdir
+# cd $qtmoko_dir/src/3rdparty/applications/eyepiece && dpkg-buildpackage -tc
+# mv $qtmoko_dir/src/3rdparty/applications/qtmoko-eyepiece_*-* $outdir
 
 echo "==================================================== Building qtmoko-gqsync.deb"
-cd $qtmoko_dir/qtmoko-apps/gqsync && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-gqsync_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/gqsync && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-gqsync_*-* $outdir
 
 echo "=============================================== Building qtmoko-greensudoku.deb"
-cd $qtmoko_dir/qtmoko-apps/greensudoku && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-greensudoku_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/games/greensudoku && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/games/qtmoko-greensudoku_*-* $outdir
 
 echo "================================================= Building qtmoko-keypebble.deb"
-cd $qtmoko_dir/qtmoko-apps/keypebble && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-keypebble_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/keypebble && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-keypebble_*-* $outdir
 
 echo "================================================== Building qtmoko-phonetiq.deb"
-cd $qtmoko_dir/qtmoko-apps/phonetiq && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-phonetiq_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/phonetiq && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-phonetiq_*-* $outdir
 
 echo "=============================================== Building qtmoko-qmokoplayer.deb"
-cd $qtmoko_dir/qtmoko-apps/qmokoplayer && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-qmokoplayer_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/qmokoplayer && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-qmokoplayer_*-* $outdir
 
 echo "================================================= Building qtmoko-solitaire.deb"
-cd $qtmoko_dir/qtmoko-apps/qsolitaire && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-solitaire_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/games/qsolitaire && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/games/qtmoko-solitaire_*-* $outdir
 
 echo "================================================= Building qtmoko-qtopiagps.deb"
-cd $qtmoko_dir/qtmoko-apps/qtopiagps && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-qtopiagps_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/qtopiagps && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-qtopiagps_*-* $outdir
 
 echo "================================================== Building qtmoko-qweather.deb"
-cd $qtmoko_dir/qtmoko-apps/qweather && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-qweather_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/qweather && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-qweather_*-* $outdir
 
 echo "================================================== Building qtmoko-spectemu.deb"
-cd $qtmoko_dir/qtmoko-apps/spectemu && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-spectemu_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/spectemu && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-spectemu_*-* $outdir
 
 echo "==================================================== Building qtmoko-gqsync.deb"
-cd $qtmoko_dir/qtmoko-apps/gqsync && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-gqsync_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/gqsync && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-gqsync_*-* $outdir
 
 echo "==================================================== Building qtmoko-mqutim.deb"
-cd $qtmoko_dir/qtmoko-apps/mqutim && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-mqutim_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/mqutim && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-mqutim_*-* $outdir
 
 echo "=================================================== Building qtmoko-noxchat.deb"
-cd $qtmoko_dir/qtmoko-apps/noxchat && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-noxchat_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/noxchat && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-noxchat_*-* $outdir
 
 echo "================================================ Building qtmoko-qalsamixer.deb"
-cd $qtmoko_dir/qtmoko-apps/qalsamixer && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-qalsamixer_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/qalsamixer && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-qalsamixer_*-* $outdir
 
 echo "================================================== Building qtmoko-dictopia.deb"
-cd $qtmoko_dir/qtmoko-apps/qdictopia && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-dictopia_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/qdictopia && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-dictopia_*-* $outdir
 
 echo "========================================== Building qtmoko-qgoogletranslate.deb"
-cd $qtmoko_dir/qtmoko-apps/qgoogletranslate && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-qgoogletranslate_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/qgoogletranslate && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-qgoogletranslate_*-* $outdir
 
 echo "================================================== Building qtmoko-qneoroid.deb"
-cd $qtmoko_dir/qtmoko-apps/qneoroid && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-qneoroid_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/qneoroid && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-qneoroid_*-* $outdir
 
 echo "================================================== Building qtmoko-qtbackup.deb"
-cd $qtmoko_dir/qtmoko-apps/qtbackup && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-qtbackup_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/qtbackup && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-qtbackup_*-* $outdir
 
 echo "=============================================== Building qtmoko-qtpedometer.deb"
-cd $qtmoko_dir/qtmoko-apps/qtpedometer && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-qtpedometer_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/qtpedometer && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-qtpedometer_*-* $outdir
 
 echo "=================================================== Building qtmoko-shopper.deb"
-cd $qtmoko_dir/qtmoko-apps/shopper && dpkg-buildpackage -tc
-mv $qtmoko_dir/qtmoko-apps/qtmoko-shopper_*-* $outdir
+cd $qtmoko_dir/src/3rdparty/applications/shopper && dpkg-buildpackage -tc
+mv $qtmoko_dir/src/3rdparty/applications/qtmoko-shopper_*-* $outdir
 
 echo "============================================= Packages should be now in $outdir"
