319dac11c90a667c58e64cbf03e4dd5e45fa1886
Radek Polak <psonek2@seznam.cz>
2011-07-03 22:16:46 +0200

    FSO - call device status

--- src/server/phone/telephony/phoneserver/fsoservice/fsotelephonyservice.cpp
+++ src/server/phone/telephony/phoneserver/fsoservice/fsotelephonyservice.cpp
@@ -52,8 +52,8 @@ FsoTelephonyService::FsoTelephonyService(const QString& service, QObject *parent
             this,
             SLOT(incomingMessageReport(int, const QString &, const QString &, const QString &)));
     
-    //QFsoDBusPendingCall call = gsmDev.GetDeviceStatus();
-    //watchFsoCall(call, this, SLOT(getDeviceStatusFinished(QFsoDBusPendingCall &)));
+    QFsoDBusPendingCall call = gsmDev.GetDeviceStatus();
+    watchFsoCall(call, this, SLOT(getDeviceStatusFinished(QFsoDBusPendingCall &)));
 }
 
 FsoTelephonyService::~FsoTelephonyService()
