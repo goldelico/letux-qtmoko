dca3eae0c70c837a62f73f78863eb07b98b88d30
Radek Polak <psonek2@seznam.cz>
2011-08-15 16:05:53 +0200

    Scipt for indenting source files

---
+++ scripts/indent.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# I am using this script to format qtmoko source code. You will need to install
+# patched GNU indent (it's under src/tools/indent).
+indent -nbad -bap -nbad -nbc -bbo -hnl -br -bls -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i8 -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il0 $@
+
+
+#kdialog --title Test -msgbox "$@"
