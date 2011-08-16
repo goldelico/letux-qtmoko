77474ca9b65e2222e82ce151753b4adce0c7660b
Radek Polak <psonek2@seznam.cz>
2011-08-16 13:45:04 +0200

    indent.sh - fix spaces in class definition

--- scripts/indent.sh
+++ scripts/indent.sh
@@ -3,5 +3,9 @@
 # patched GNU indent (it's under src/tools/indent).
 indent -nut -nbad -bap -nbad -nbc -bbo -hnl -br -bls -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i4 -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts4 -il0 $@
 
+# Correctly indent class definition e.g.
+# class FsoSMSReader:public QSMSReader becomes
+# class FsoSMSReader : public QSMSReader
+sed -i 's/:public / : public /' $1
 
 #kdialog --title Test -msgbox "$@"
