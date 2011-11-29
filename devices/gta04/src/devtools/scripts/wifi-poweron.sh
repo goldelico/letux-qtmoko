3498231cacf14564590b24db5a93251cc14f85be
Radek Polak <psonek2@seznam.cz>
2011-11-29 22:55:13 +0100

    gta04 wifi-poweron - just turn the power - we have fw in kernel now

--- devices/gta04/src/devtools/scripts/wifi-poweron.sh
+++ devices/gta04/src/devtools/scripts/wifi-poweron.sh
@@ -1,24 +1,7 @@
 #!/bin/sh
-
-# Start temporarily udevd so that it provides firmware for the wifi
-udevd --debug &
-pid=$!
-
-sleep 1
-
 modprobe libertas_sdio
-
 echo "255" >/sys/class/leds/tca6507:6/brightness # activate reset
-echo "2800000" >/sys/devices/platform/reg-virt-consumer.4/max_microvolts
-echo "2800000" >/sys/devices/platform/reg-virt-consumer.4/min_microvolts
+echo "3150000" >/sys/devices/platform/reg-virt-consumer.4/max_microvolts
+echo "3150000" >/sys/devices/platform/reg-virt-consumer.4/min_microvolts
 echo "normal" >/sys/devices/platform/reg-virt-consumer.4/mode
 echo "0" >/sys/class/leds/tca6507:6/brightness # release reset
-
-sleep 3
-
-# We can stop udevd now
-kill -TERM $pid
-
-sleep 1
-
-ifconfig wlan0 up
