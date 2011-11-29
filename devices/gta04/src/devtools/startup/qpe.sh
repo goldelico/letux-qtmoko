40dee08e2abca0db3c01b7a3c59652dda0a1f45a
Radek Polak <psonek2@seznam.cz>
2011-11-29 23:04:01 +0100

    gta04 qpe.sh - remove gta02 stuff

--- devices/gta04/src/devtools/startup/qpe.sh
+++ devices/gta04/src/devtools/startup/qpe.sh
@@ -9,7 +9,6 @@ if [ ! -d /dev/pts ]; then
 fi
 rm -f /dev/input/mice
 echo '1' > /proc/sys/kernel/printk
-echo 1 > /sys/devices/platform/gta02-pm-bt.0/power_on
 echo 0 > /sys/devices/virtual/graphics/fbcon/cursor_blink
 stty -F /dev/tty1 -echo
 rm -rf /var/run/ppp
@@ -20,12 +19,4 @@ atd /var/spool/at
 touch /tmp/restart-qtopia
 while [ -e /tmp/restart-qtopia ]; do
 qpe 2>&1 | logger -t 'Qtopia'
-if [ -e /tmp/restart-qtopia-qvga ]; then
-    rm -rf /tmp/restart-qtopia-qvga
-    export QWS_SIZE="240x320+0+0"
-    export POINTERCAL_FILE=/etc/pointercal_qvga
-    fbset qvga
-    echo qvga > /sys/bus/spi/devices/spi2.0/resolution
-    #qcop service send Launcher "execute(QString)" "calibrate"
-fi
 done
