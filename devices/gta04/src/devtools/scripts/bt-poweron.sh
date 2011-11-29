67c20769a7655787def05a3f8ff832150fd5eaac
Radek Polak <psonek2@seznam.cz>
2011-11-29 23:19:23 +0100

    gta04 bt-poweron - hciattach, start bluetoothd

--- devices/gta04/src/devtools/scripts/bt-poweron.sh
+++ devices/gta04/src/devtools/scripts/bt-poweron.sh
@@ -4,3 +4,9 @@ echo "2800000" >/sys/devices/platform/reg-virt-consumer.4/max_microvolts
 echo "2800000" >/sys/devices/platform/reg-virt-consumer.4/min_microvolts
 echo "normal" >/sys/devices/platform/reg-virt-consumer.4/mode
 echo "0" >/sys/class/leds/tca6507:6/brightness # release reset
+
+hciattach -s 115200 /dev/ttyS0 any 115200 flow
+/etc/init.d/bluetooth start
+
+echo "Please restart QtMoko to make bluetooth working"
+sleep 5
