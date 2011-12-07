0c79f894ef9a58c8bfa7431a4c33d635a3f26e22
Gilles Filippini <pini@debian.org>
2011-12-07 22:02:04 +0100

    Remove unused debian files from dh_make template

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(qtmoko):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="qtmoko" command="/usr/bin/qtmoko"
--- debian/qtmoko.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the qtmoko package
-#
-0 4	* * *	root	[ -x /usr/bin/qtmoko_maintenance ] && /usr/bin/qtmoko_maintenance
