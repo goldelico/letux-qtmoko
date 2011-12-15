7b3db590f332a6011417d22cd21a638fd616179f
Radek Polak <psonek2@seznam.cz>
2011-12-15 21:01:48 +0000

    gta04 - wifi-poweron for latest 3.2 kernel
    http://lists.goldelico.com/pipermail/gta04-owner/2011-December/000967.html

--- devices/gta04/src/devtools/scripts/wifi-poweron.sh
+++ devices/gta04/src/devtools/scripts/wifi-poweron.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+udevd &         # start udevd to supply firmware for wifi
+sleep 3
 modprobe libertas_sdio
-echo "255" >/sys/class/leds/tca6507:6/brightness # activate reset
-echo "3150000" >/sys/devices/platform/reg-virt-consumer.4/max_microvolts
-echo "3150000" >/sys/devices/platform/reg-virt-consumer.4/min_microvolts
+VDD=3150000
+# VDD=2800000
+echo 1 > /sys/class/gpio/gta04:wlan:cd/value
+echo "$VDD" >/sys/devices/platform/reg-virt-consumer.4/max_microvolts
+echo "$VDD" >/sys/devices/platform/reg-virt-consumer.4/min_microvolts
 echo "normal" >/sys/devices/platform/reg-virt-consumer.4/mode
-echo "0" >/sys/class/leds/tca6507:6/brightness # release reset
+echo 0 > /sys/class/gpio/gta04:wlan:cd/value  # edge triggers interrupt.
