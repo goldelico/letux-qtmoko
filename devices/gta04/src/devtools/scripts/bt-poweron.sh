b843ef2a8e9d9610a892133d40b18c3b0c7a254a
Radek Polak <psonek2@seznam.cz>
2012-01-20 18:32:52 +0000

    gta04 bt - set faster speed

--- devices/gta04/src/devtools/scripts/bt-poweron.sh
+++ devices/gta04/src/devtools/scripts/bt-poweron.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
-echo "255" >/sys/class/leds/tca6507:6/brightness # activate reset
-echo "2800000" >/sys/devices/platform/reg-virt-consumer.4/max_microvolts
-echo "2800000" >/sys/devices/platform/reg-virt-consumer.4/min_microvolts
+VDD=3150000
+# VDD=2800000
+echo 1 > /sys/class/gpio/gta04:wlan:cd/value
+echo "$VDD" >/sys/devices/platform/reg-virt-consumer.4/max_microvolts
+echo "$VDD" >/sys/devices/platform/reg-virt-consumer.4/min_microvolts
 echo "normal" >/sys/devices/platform/reg-virt-consumer.4/mode
-echo "0" >/sys/class/leds/tca6507:6/brightness # release reset
+echo 0 > /sys/class/gpio/gta04:wlan:cd/value  # edge triggers interrupt.
 
-hciattach -s 115200 /dev/ttyS0 any 115200 flow
+hciattach -s 115200 /dev/ttyO0 any 115200 flow
+hciconfig hci0 up
+bccmd -d hci0 psset baudrate 12288
+bccmd -d hci0 warmreset
+killall hciattach
+sleep 3
+hciattach -s 3000000 /dev/ttyO0 any 3000000 flow
+hciconfig hci0 up
+
 /etc/init.d/bluetooth start
 
 echo "Please restart QtMoko to make bluetooth working"
