ef1a492f2dd1f7e14829803a9bf273cd3b175380
Radek Polak <psonek2@seznam.cz>
2012-03-08 23:29:32 +0000

    blink with green led on missed call

--- src/libraries/qtopiaphonemodem/qmodemllindicators.cpp
+++ src/libraries/qtopiaphonemodem/qmodemllindicators.cpp
@@ -25,42 +25,50 @@
 // Turn on/off led. Device can be "gta02" or "gta04".
 // GTA04: color can be "green" or "red" type can be "power" or "aux"
 // GTA02: supports "blue power" "orange power" and "red aux"
-static void setLed(const char *device, const char *color,
-                   const char *type, bool value)
+static bool setLed(const char *device, const char *color,
+                   const char *type, const char *attr, const char *value)
 {
     char filename[255];
-    sprintf(filename, "/sys/class/leds/%s:%s:%s/brightness", device, color, type);  // e.g. /sys/class/leds/gta04:green:power
+    sprintf(filename, "/sys/class/leds/%s:%s:%s/%s", device, color, type, attr);    // e.g. /sys/class/leds/gta04:green:power/brightness
     QFile f(filename);
     if (!f.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Truncate)) {
         qWarning() << "setLed failed" << f.errorString();
-        return;
+        return false;
     }
-    f.write(value ? "255" : "0");
+    f.write(value);
     f.close();
+    return true;
 }
 
 #ifdef QT_QWS_GTA04
 
 static void missedCallLedOn()
 {
-    setLed("gta04", "green", "power", true);
+    static bool blinkOk = false;
+    setLed("gta04", "green", "power", "brightness", "255");
+    if (blinkOk) {
+        return;
+    }
+    blinkOk = setLed("gta04", "green", "power", "trigger", "timer") &&
+        setLed("gta04", "green", "power", "delay_off", "1024") &&
+        setLed("gta04", "green", "power", "delay_on", "1024");
 }
 
 static void missedCallLedOff()
 {
-    setLed("gta04", "green", "power", false);
+    setLed("gta04", "green", "power", "brightness", "0");
 }
 
 #elif QT_QWS_NEO
 
 static void missedCallLedOn()
 {
-    setLed("gta02", "blue", "power", true);
+    setLed("gta02", "blue", "power", "brightness", "255");
 }
 
 static void missedCallLedOff()
 {
-    setLed("gta02", "blue", "power", false);
+    setLed("gta02", "blue", "power", "brightness", "0");
 }
 
 #else
@@ -88,6 +96,8 @@ void QModemLLIndicators::MissedCallsChanged(int num)
     qLog(Modem) << "MissedCallsChanged " << numMissed << "->" << num;
     if (numMissed > 0 && num == 0) {
         missedCallLedOff();     // clear missed call led
+    } else if (numMissed == 0 && num > 0) {
+        missedCallLedOn();
     }
     numMissed = num;
 }
--- src/server/phone/telephony/phoneserver/ofonoservice/llindicators.cpp
+++ src/server/phone/telephony/phoneserver/ofonoservice/llindicators.cpp
@@ -24,42 +24,50 @@
 // Turn on/off led. Device can be "gta02" or "gta04".
 // GTA04: color can be "green" or "red" type can be "power" or "aux"
 // GTA02: supports "blue power" "orange power" and "red aux"
-static void setLed(const char *device, const char *color,
-                   const char *type, bool value)
+static bool setLed(const char *device, const char *color,
+                   const char *type, const char *attr, const char *value)
 {
     char filename[255];
-    sprintf(filename, "/sys/class/leds/%s:%s:%s/brightness", device, color, type); // e.g. /sys/class/leds/gta04:green:power
+    sprintf(filename, "/sys/class/leds/%s:%s:%s/%s", device, color, type, attr);    // e.g. /sys/class/leds/gta04:green:power/brightness
     QFile f(filename);
     if (!f.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Truncate)) {
         qWarning() << "setLed failed" << f.errorString();
-        return;
+        return false;
     }
-    f.write(value ? "255" : "0");
+    f.write(value);
     f.close();
+    return true;
 }
 
 #ifdef QT_QWS_GTA04
 
 static void missedCallLedOn()
 {
-    setLed("gta04", "green", "power", true);
+    static bool blinkOk = false;
+    setLed("gta04", "green", "power", "brightness", "255");
+    if (blinkOk) {
+        return;
+    }
+    blinkOk = setLed("gta04", "green", "power", "trigger", "timer") &&
+        setLed("gta04", "green", "power", "delay_off", "1024") &&
+        setLed("gta04", "green", "power", "delay_on", "1024");
 }
 
 static void missedCallLedOff()
 {
-    setLed("gta04", "green", "power", false);
+    setLed("gta04", "green", "power", "brightness", "0");
 }
 
 #elif QT_QWS_NEO
 
 static void missedCallLedOn()
 {
-    setLed("gta02", "blue", "power", true);
+    setLed("gta02", "blue", "power", "brightness", "255");
 }
 
 static void missedCallLedOff()
 {
-    setLed("gta02", "blue", "power", false);
+    setLed("gta02", "blue", "power", "brightness", "0");
 }
 
 #else
@@ -87,6 +95,8 @@ void llIndicatorMissedCallsChanged(int num)
     qDebug() << "MissedCallsChanged " << numMissed << "->" << num;
     if (numMissed > 0 && num == 0) {
         missedCallLedOff();     // clear missed call led
+    } else if (numMissed == 0 && num > 0) {
+        missedCallLedOn();
     }
     numMissed = num;
 }
@@ -94,11 +104,11 @@ void llIndicatorMissedCallsChanged(int num)
 // Called on OFonoPhoneCall.hangup()
 void llIndicatorsVoiceCallHangup()
 {
-    missedCallLedOff();           // clear missed call led when user hangs up
+    missedCallLedOff();         // clear missed call led when user hangs up
 }
 
 // Called on OFonoPhoneCall.accept()
 void llIndicatorsVoiceCallAccept()
 {
-    missedCallLedOff();           // clear missed call led when users accepts the call
+    missedCallLedOff();         // clear missed call led when users accepts the call
 }
