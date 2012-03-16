ff19884268e1c13a6c530001f9a74c10d9f8456a
Radek Polak <psonek2@seznam.cz>
2012-03-16 13:17:54 +0100

    bt-poweron - remove old 2.6.32 code

--- devices/gta04/src/devtools/scripts/bt-poweron.sh
+++ devices/gta04/src/devtools/scripts/bt-poweron.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-VDD=3150000
-# VDD=2800000
-echo 1 > /sys/class/gpio/gta04:wlan:cd/value
-echo "$VDD" >/sys/devices/platform/reg-virt-consumer.4/max_microvolts
-echo "$VDD" >/sys/devices/platform/reg-virt-consumer.4/min_microvolts
-echo "normal" >/sys/devices/platform/reg-virt-consumer.4/mode
-echo 0 > /sys/class/gpio/gta04:wlan:cd/value  # edge triggers interrupt.
-
 hciattach -s 115200 /dev/ttyO0 any 115200 flow
 hciconfig hci0 up
 bccmd -d hci0 psset baudrate 12288
