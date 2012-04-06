514907108163af93f82112cf20892bbeb6e20d9d
Radek Polak <psonek2@seznam.cz>
2012-04-06 10:47:09 +0200

    qmplayer - reformat code with indent.sh

--- src/3rdparty/applications/qmplayer/qmplayer.h
+++ src/3rdparty/applications/qmplayer/qmplayer.h
@@ -37,27 +37,23 @@
 
 #define NUM_MENU_ITEMS 3
 
-
-
 // This is simple fullscreen widget used for refreshing softmenu bar and top
 // bar. It's also base for the playing full screen widget
 class QMplayerFullscreen : public QWidget
 {
     Q_OBJECT
-
 public:
     QMplayerFullscreen();
     bool clicked;
     QPixmap pixmap;
 
-signals:
-    void deactivated();
+     signals:void deactivated();
 
 public slots:
     void showScreen();
 
 protected:
-    bool event(QEvent *);
+     bool event(QEvent *);
     void enterFullScreen();
 };
 
@@ -67,12 +63,10 @@ protected:
 class QMplayerFullscreenPlay : public QMplayerFullscreen
 {
     Q_OBJECT
-
 public:
     QMplayerFullscreenPlay();
 
-signals:
-    void pause();
+    signals: void pause();
     void volumeUp();
     void volumeDown();
 
@@ -90,9 +84,8 @@ private:
 class QMplayer : public QWidget
 {
     Q_OBJECT
-
 public:
-    QMplayer(QWidget *parent = 0, Qt::WFlags f = 0);
+    QMplayer(QWidget * parent = 0, Qt::WFlags f = 0);
     ~QMplayer();
 
     enum Screen
@@ -110,32 +103,32 @@ public:
     };
 
 private:
-    Screen screen;
+     Screen screen;
     int maxScanLevel;
     int delTmpFiles;
     bool abort;
     int useBluetooth;
-    QMplayerFullscreenPlay fsPlay;      // fulscreen used for playing
-    QMplayerFullscreen fsRefresh;       // fullscreen used to refresh upper bar and softmenubar
-    QVBoxLayout* layout;
-    QHBoxLayout* buttonLayout;
-    QListWidget* lw;
+    QMplayerFullscreenPlay fsPlay;  // fulscreen used for playing
+    QMplayerFullscreen fsRefresh;   // fullscreen used to refresh upper bar and softmenubar
+    QVBoxLayout *layout;
+    QHBoxLayout *buttonLayout;
+    QListWidget *lw;
     QListWidgetItem *scanItem;
     QListWidgetItem *sharingItem;
     QListWidgetItem *encodingItem;
-    QLabel* label;
+    QLabel *label;
     QLineEdit *lineEdit;
-    QPushButton* bOk;
-    QPushButton* bBack;
-    QPushButton* bUp;
-    QPushButton* bDown;
-    QProcess* process;
+    QPushButton *bOk;
+    QPushButton *bBack;
+    QPushButton *bUp;
+    QPushButton *bDown;
+    QProcess *process;
     QProgressBar *progress;
     QTcpServer *tcpServer;
     QStringList mpargs;
     QStringList mplist;
 #ifdef QTOPIA
-    QMenu* softm;
+    QMenu *softm;
     QAction *rmMpAction, *rmDlAction, *rmFlvAction;
 #endif
     bool mpgui;
@@ -148,7 +141,8 @@ private:
 
     void showScreen(QMplayer::Screen scr);
     void scan();
-    int scanDir(QString const& path, int level, int maxLevel, int min, int max, bool followSymLinks);
+    int scanDir(QString const &path, int level, int maxLevel, int min, int max,
+                bool followSymLinks);
     void sharing();
     void play(QStringList & args);
     void encode(QString filename);
@@ -157,18 +151,19 @@ private:
     bool installYoutubeDl();
     bool runServer();
     bool runClient();
-    bool download(QString url, QString destPath, QString filename, bool justCheck);
+    bool download(QString url, QString destPath, QString filename,
+                  bool justCheck);
     bool startMencoder(QString srcFile, QString dstFile);
     void console(QString s);
     bool youtubeDl();
-    bool runCmd(QString cmd, int maxp=0);
+    bool runCmd(QString cmd, int maxp = 0);
     void setDlText();
     QString getEncFilename(QString srcFile, QString dstIdentifier);
     bool isPlaylist(QString fileName);
 
 protected:
     void paintEvent(QPaintEvent *);
-    void closeEvent(QCloseEvent *event);
+    void closeEvent(QCloseEvent * event);
 
 private slots:
     void okClicked();
@@ -194,8 +189,8 @@ private slots:
 class QMplayerMainWindow : public QMainWindow
 {
 public:
-    QMplayerMainWindow(QWidget *parent = 0, Qt::WFlags f = 0);
+    QMplayerMainWindow(QWidget * parent = 0, Qt::WFlags f = 0);
     ~QMplayerMainWindow();
 };
 
-#endif // QMPLAYER_H
+#endif                          // QMPLAYER_H
