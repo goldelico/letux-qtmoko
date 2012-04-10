5a22023d078abe44c630ab028f4b61e6ba54ceb0
Radek Polak <psonek2@seznam.cz>
2012-04-10 22:55:44 +0000

    gta04 bluetooth - power it of after 30s when qtmoko is started
    for some reason if we power it down immediately in qpe.sh rfkill
    in bt settings stops working.

--- devices/gta04/src/devtools/scripts/bt-poweron.sh
+++ devices/gta04/src/devtools/scripts/bt-poweron.sh
@@ -1,14 +1,2 @@
 #!/bin/sh
-hciattach -s 115200 /dev/ttyO0 any 115200 flow
-hciconfig hci0 up
-bccmd -d hci0 psset baudrate 12288
-bccmd -d hci0 warmreset
-killall hciattach
-sleep 3
-hciattach -s 3000000 /dev/ttyO0 any 3000000 flow
-hciconfig hci0 up
-
-/etc/init.d/bluetooth start
-
-echo "Please restart QtMoko to make bluetooth working"
-sleep 5
+rfkill unblock bluetooth
