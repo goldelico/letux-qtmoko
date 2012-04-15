da9c6119c7eb616c2bb680a227770d2e5e55b9ea
Radek Polak <psonek2@seznam.cz>
2012-04-15 12:24:40 +0000

    gta04 - better scripts for modem power on/off

--- devices/gta04/src/devtools/scripts/modem-poweron.sh
+++ devices/gta04/src/devtools/scripts/modem-poweron.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 if [ -f /sys/class/gpio/gpio186/value ];
 then
-    for repeat in 1 2 3 4 5
+    for i in $(seq 0 1 299)
     do
         if [ -e /dev/ttyHS_Application ];
         then
-                break
+                exit 0   # modem ok
         fi
-        echo 1 >/sys/devices/virtual/gpio/gpio186/value
-        sleep 1
-        echo 0 >/sys/devices/virtual/gpio/gpio186/value
-        sleep 5
+        if [  $(( $i % 50 )) -eq 0 ];
+        then
+            echo 1 >/sys/devices/virtual/gpio/gpio186/value
+            sleep 0.2
+            echo 0 >/sys/devices/virtual/gpio/gpio186/value
+        else
+            sleep 0.1
+        fi
     done
+else
+    exit 0  # gta04a03
 fi
+echo "failed to power on modem"
+exit 1
