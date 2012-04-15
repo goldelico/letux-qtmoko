bd0df55d841e2af3f1cf0ca7db906c5f0254400c
Radek Polak <psonek2@seznam.cz>
2012-04-15 10:11:58 +0000

    gta04 - scripts for modem power on and power off
    Based on this mail:
    
        http://lists.goldelico.com/pipermail/gta04-owner/2012-April/002238.html

---
+++ devices/gta04/src/devtools/scripts/modem-poweron.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ -f /sys/class/gpio/gpio186/value ];
+then
+    for repeat in 1 2 3 4 5
+    do
+        if [ -e /dev/ttyHS_Application ];
+        then
+                break
+        fi
+        echo 1 >/sys/devices/virtual/gpio/gpio186/value
+        sleep 1
+        echo 0 >/sys/devices/virtual/gpio/gpio186/value
+        sleep 5
+    done
+fi
