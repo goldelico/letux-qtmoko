8e013fa76c979fe38e066ffc6876de3673d7ef3f
Radek Polak <psonek2@seznam.cz>
2012-11-17 16:07:57 +0100

    version.h - bump to v50

--- src/libraries/qtopiabase/version.h
+++ src/libraries/qtopiabase/version.h
@@ -20,8 +20,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define QTMOKO_VERSION 49
-#define QTMOKO_VERSION_STR "49"
+#define QTMOKO_VERSION 50
+#define QTMOKO_VERSION_STR "50"
 
 // Legacy define. Present since 1.x.
 #define QPE_VERSION "4.4.3"
