03bb2e157b6e36afd10a5702f1976f688c4060d1
Neil Jerram <neil@ossau.homelinux.net>
2012-11-24 10:33:15 +0100

    Prevent pulseaudio from exiting every 20 seconds
    Also do some logging, which I think will be useful.

--- devices/gta04/src/devtools/scripts/pulse.sh
+++ devices/gta04/src/devtools/scripts/pulse.sh
@@ -4,6 +4,6 @@ then
 	while [ 1 = 1  ]
 	do
 		logger -t pulse.sh "Starting pulseaudio"
-		pulseaudio 2>&1 | logger -t pulse.sh
+		pulseaudio --log-level=info --exit-idle-time=-1 2>&1 | logger -t pulse.sh
 	done
 fi
