725bc7a6f041bbbbf6fd76d2116265aac2977bf8
Radek Polak <psonek2@seznam.cz>
2012-12-07 11:01:36 +0100

    Include also dmesg in modem reenumerate log
    thanks to Nikolaus for the idea and command

--- devices/gta04/src/devtools/scripts/fix-modem-reenumerate.sh
+++ devices/gta04/src/devtools/scripts/fix-modem-reenumerate.sh
@@ -6,6 +6,8 @@ echo ""
 
 echo "$(date) modem reenumerated" >> /modem_reenumerate.log
 
+dmesg | tail -n 50 >>/modem_reenumerate.log
+
 for i in 10 9 8 7 6 5 4 3 2 1
 do
     echo "rebooting in $i seconds"
