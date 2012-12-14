2bccc23bf0b059ea9005a20e02497e4bd66ff88e
Radek Polak <psonek2@seznam.cz>
2012-12-14 23:19:28 +0100

    fix tuxpain launching

--- src/3rdparty/games/qtmoko-tuxpaint/tuxpaint.sh
+++ src/3rdparty/games/qtmoko-tuxpaint/tuxpaint.sh
@@ -4,5 +4,5 @@ export SDL_VIDEODRIVER=QtMoko
 export SDL_AUDIODRIVER=pulse
 #export SDL_QT_DEBUG=1
 export SDL_QT_FS=1
-tuxpaint --480x640
+tuxpaint --640x480
 qcop service send RotationManager "setCurrentRotation(int)" 0
