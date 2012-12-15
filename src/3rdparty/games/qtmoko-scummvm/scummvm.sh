bcb3f9275b254cba444531dbfe940a90b4786d24
Radek Polak <psonek2@seznam.cz>
2012-12-15 11:43:28 +0100

    sdl games - use ~/.sdlnosound instead of duplicated scripts/desktops

--- src/3rdparty/games/qtmoko-scummvm/scummvm.sh
+++ src/3rdparty/games/qtmoko-scummvm/scummvm.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 qcop service send RotationManager "setCurrentRotation(int)" 90
 export SDL_VIDEODRIVER=QtMoko
-export SDL_AUDIODRIVER=pulse
+if [ -f ~/.sdlnosound ]
+then
+    export SDL_AUDIODRIVER=dummy
+else
+    export SDL_AUDIODRIVER=pulse
+fi
 #export SDL_QT_DEBUG=1
 export SDL_QT_FS=1
 export SDL_QT_SCRIPT=/opt/qtmoko/share/sdl/scummvm.js
--- src/3rdparty/games/qtmoko-tuxpaint/tuxpaint.sh
+++ src/3rdparty/games/qtmoko-tuxpaint/tuxpaint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 qcop service send RotationManager "setCurrentRotation(int)" 90
 export SDL_VIDEODRIVER=QtMoko
-export SDL_AUDIODRIVER=pulse
+if [ -f ~/.sdlnosound ]
+then
+    export SDL_AUDIODRIVER=dummy
+else
+    export SDL_AUDIODRIVER=pulse
+fi
 #export SDL_QT_DEBUG=1
 export SDL_QT_FS=1
 tuxpaint --640x480
