aecb14a1813a60fad648e1cfb8d650e15e130034
Radek Polak <psonek2@seznam.cz>
2012-12-27 22:28:59 +0100

    use UNSIGNED BIG INT also for status in mailfolders table

--- src/libraries/qtopiamail/resources/mailfolders.sqlite.sql
+++ src/libraries/qtopiamail/resources/mailfolders.sqlite.sql
@@ -3,7 +3,7 @@ CREATE TABLE mailfolders (
     name VARCHAR(255) NOT NULL,
     parentid INTEGER,
     parentaccountid INTEGER,
-    status INTEGER,
+    status UNSIGNED BIG INT,
     displayname VARCHAR(255),
     FOREIGN KEY (parentaccountid) REFERENCES mailaccounts(id),
     FOREIGN KEY (parentid) REFERENCES mailfolder(id));
