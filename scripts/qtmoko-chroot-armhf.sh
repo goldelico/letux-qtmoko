322d636429b1abfd8cfd006df2e922b5516bf1a3
Radek Polak <psonek2@seznam.cz>
2013-03-01 10:49:21 +0100

    qtmoko-chroot-armhf.sh - hack to install libpulsecommon

--- scripts/qtmoko-chroot-armhf.sh
+++ scripts/qtmoko-chroot-armhf.sh
@@ -81,10 +81,16 @@ apt-get install g++-4.6-arm-linux-gnueabihf
 echo "Installing xapt and ARM qtmoko dependencies"
 apt-get install xapt
 xapt -a armhf -m libxext-dev libasound2-dev libdbus-1-dev libssl-dev libts-dev libbluetooth-dev libxtst-dev libpng12-dev libjpeg8-dev libv4l-dev libspeexdsp-dev libglib2.0-dev libsqlite3-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libvorbis-dev libpulse-dev libssl-dev
-
 pushd /usr/arm-linux-gnueabihf/lib/
 ln -s ld-linux-armhf.so.3 ld-linux.so.3
 popd
+
+# For some reason libpulsecommon is not fetched by xapt, so do it manually
+cd /tmp
+curl -O http://ftp.de.debian.org/debian/pool/main/p/pulseaudio/libpulse0_2.0-6_armhf.deb
+dpkg -x libpulse0_2.0-6_armhf.deb .
+cp usr/lib/arm-linux-gnueabihf/pulseaudio/libpulsecommon-2.0.so /usr/arm-linux-gnueabihf/lib/
+
 
 echo "export PATH=/usr/lib/ccache:\$PATH" >> /root/.bashrc
 echo "PS1='qtmoko-chroot:\w\\\$ '" >> /root/.bashrc
