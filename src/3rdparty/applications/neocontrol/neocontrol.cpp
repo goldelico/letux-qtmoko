a00091b6253ae75d20966f88e6cae18b5d5946c5
Radek Polak <psonek2@seznam.cz>
2013-12-01 17:52:17 +0100

    neocontrol gta02 - modem power_on path for 2.6.39

--- src/3rdparty/applications/neocontrol/neocontrol.cpp
+++ src/3rdparty/applications/neocontrol/neocontrol.cpp
@@ -485,7 +485,7 @@ void NeoControl::updateSysfs()
     QString text;
     appendValue(tr("Battery status"), "/sys/class/power_supply/battery/status", &text);
     appendValue(tr("Current"), "/sys/class/power_supply/battery/current_now", &text);
-    appendValue(tr("Modem power"), "/sys/devices/platform/s3c2440-i2c/i2c-0/0-0073/pcf50633-gpio.0/reg-fixed-voltage.1/gta02-pm-gsm.0/power_on", &text);
+    appendValue(tr("Modem power"), "/sys/devices/platform/s3c2440-i2c/i2c-0/0-0073/pcf50633-gpio/reg-fixed-voltage.1/gta02-pm-gsm.0/power_on", &text);
     appendValue(tr("GPS power"), "/sys/devices/platform/gta02-pm-gps.0/power_on", &text);
     appendValue(tr("Bluetooth power"), "/sys/devices/platform/gta02-pm-bt.0/power_on", &text);
     label->setText(text);
