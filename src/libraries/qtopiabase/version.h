35dad39569f99461a2ef368853455d4c89e61688
Radek Polak <psonek2@seznam.cz>
2013-12-01 21:17:19 +0100

    changelog and v58

--- src/libraries/qtopiabase/version.h
+++ src/libraries/qtopiabase/version.h
@@ -20,8 +20,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define QTMOKO_VERSION 57
-#define QTMOKO_VERSION_STR "57"
+#define QTMOKO_VERSION 58
+#define QTMOKO_VERSION_STR "58"
 
 // Legacy define. Present since 1.x.
 #define QPE_VERSION "4.4.3"
