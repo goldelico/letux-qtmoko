fc38f3406c519b6d4addd263a2cfd7b9bd49edeb
Andreas Kemnade <andreas@kemnade.info>
2017-12-26 20:54:36 +0100

    modem pm: do not use gpio186, rely only on rfkill
    Signed-off-by: Andreas Kemnade <andreas@kemnade.info>

--- devices/gta04/src/devtools/scripts/modem-poweroff.sh
+++ devices/gta04/src/devtools/scripts/modem-poweroff.sh
@@ -1,23 +1,3 @@
 #!/bin/sh
-if [ -f /sys/class/gpio/gpio186/value ];
-then
-    for i in $(seq 0 1 299)
-    do
-        if [ ! -e /dev/ttyHS_Application ];
-        then
-                exit 0   # modem ok
-        fi
-        if [  $(( $i % 50 )) -eq 0 ];
-        then
-            echo 1 >/sys/devices/virtual/gpio/gpio186/value
-            sleep 0.2
-            echo 0 >/sys/devices/virtual/gpio/gpio186/value
-        else
-            sleep 0.1
-        fi
-    done
-else
-    exit 0  # gta04a03
-fi
-echo "failed to power off modem"
-exit 1
+rfkill block wwan
+exit 0
